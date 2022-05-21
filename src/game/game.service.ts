7b4e1f78f09ac2c420db104d512e0b71954b689b
LHcouto <xluisx_couto@hotmail.com>
2022-05-21 14:30:33 -0300

    feat(game): create dto `CreateGameDto`

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { CreateGameDto } from "./dto/create-game.dto";
 
 
 @Injectable()
 export class GameService {
   findAll() {
     return 'Buscar todos jogos';
+  }
+
+create(createGameDto: CreateGameDto) {
+    return 'Criar um Jogo: ' + JSON.stringify(createGameDto);
   }
 }
