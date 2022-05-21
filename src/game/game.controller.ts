10e64a02c7375d3a6901ea971f40216c30b1c29f
LHcouto <xluisx_couto@hotmail.com>
2022-05-21 14:33:19 -0300

    feat(game): add `create` endpoint

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('game')
 export class GameController {
   @Get()
   findAll() {
     return 'Buscar todos os jogos';
+  }
+
+  @Post()
+  create() {
+    return 'Criar um jogo';
   }
 }
