713456ce1ad047e93cb55500994743e587ff6299
LHcouto <xluisx_couto@hotmail.com>
2022-05-21 14:53:48 -0300

    feat(game): implement create game endpoint

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from "@nestjs/common";
 import { CreateGameDto } from "./dto/create-game.dto";
+import { Game } from "./entities/game.entity";
 
 
 @Injectable()
 export class GameService {
+   games: Game[] = [];
+
+
   findAll() {
     return 'Buscar todos jogos';
   }
 
 create(createGameDto: CreateGameDto) {
-    return 'Criar um Jogo: ' + JSON.stringify(createGameDto);
+    const game: Game = { id: 'random,_id', ...createGameDto };
+
+    this.games.push(game);
+
+    return game;
   }
 }
