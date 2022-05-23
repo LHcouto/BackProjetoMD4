ffbdb703c3550ac566ff9b91b8018907d6a31d4f
LHcouto <xluisx_couto@hotmail.com>
2022-05-23 19:46:56 -0300

    feat(game): implement `update`

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,35 +1,44 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { CreateGameDto } from './dto/create-game.dto';
-import { Game } from './entities/game.entity';
-import { GameService } from './game.service';
+import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { CreateGameDto } from "./dto/create-game.dto";
+import { UpdateGameDto } from "./dto/update-game.dto";
+import { Game } from "./entities/game.entity";
+import { GameService } from "./game.service";
 
-@ApiTags('game')
-@Controller('game')
+@ApiTags("game")
+@Controller("game")
 export class GameController {
   constructor(private readonly gameService: GameService) {}
 
   @Get()
   @ApiOperation({
-    summary: 'Listar todos os jogos',
+    summary: "Listar todos os jogos",
   })
   findAll(): Promise<Game[]> {
     return this.gameService.findAll();
   }
 
-  @Get(':id')
+  @Get(":id")
   @ApiOperation({
-    summary: 'Visualizar um jogo',
+    summary: "Visualizar um jogo",
   })
-  findOne(@Param('id') id: string): Promise<Game> {
+  findOne(@Param("id") id: string): Promise<Game> {
     return this.gameService.findOne(id);
   }
 
   @Post()
   @ApiOperation({
-    summary: 'Criar um jogo',
+    summary: "Criar um jogo",
   })
   create(@Body() dto: CreateGameDto): Promise<Game> {
     return this.gameService.create(dto);
+  }
+
+  @Patch(":id")
+  @ApiOperation({
+    summary: "Editar um jogo por id",
+  })
+  update(@Param("id") id: string, @Body() dto: UpdateGameDto): Promise<Game> {
+    return this.gameService.update(id, dto);
   }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { table } from "console";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateGameDto } from "./dto/create-game.dto";
+import { UpdateGameDto } from "./dto/update-game.dto";
 import { Game } from "./entities/game.entity";
 
 
 @Injectable()
 export class GameService {
+
    constructor(private readonly prisma: PrismaService) {}
 
 
@@ -22,5 +24,14 @@ export class GameService {
     const data: Game = { ...dto };
 
     return this.prisma.game.create({ data });
+  }
+
+  update(id: string, dto: UpdateGameDto): Promise<Game> {
+    const data: Partial<Game> = {...dto};
+
+    return this.prisma.game.update({
+      where: { id },
+      data,
+    })
   }
 }
