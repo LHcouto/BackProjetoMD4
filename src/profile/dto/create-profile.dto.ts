ecd5d28c30ed84a24ebcd54d9c1c3c4d0913e1e5
Luis Couto <xluisx_couto@hotmail.com>
2022-05-29 16:36:18 -0300

    feat(profile): implement

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProfileDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsUrl } from "class-validator";
+
+export class CreateProfileDto {
+    @IsString()
+    @ApiProperty({
+      description: 'nome do perfil',
+      example: 'Luis',
+    })
+    title: string;
+
+    @IsUrl()
+    @ApiProperty({
+      description:'Imagem do perfil',
+      example: 'https://avatars.githubusercontent.com/u/98226434?v=4',
+    })
+    imageUrl: string;
+}
