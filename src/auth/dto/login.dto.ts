e1fd6420f9041b34490bf7686b05ae62b19b2bde
LHcouto <xluisx_couto@hotmail.com>
2022-06-07 22:57:13 -0300

    feat(auth): implement JWT

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,21 +1,20 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class LoginDto{
+export class LoginDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty({
     description: 'Nickname do usuário',
     example: 'luiscouto',
   })
-  nickname: string;
+  email: string;
 
   @IsString()
   @IsNotEmpty()
   @ApiProperty({
-     description: 'Senha do usário',
-     example: 'Abcd@1234',
-
+    description: 'Senha do usuário',
+    example: 'Abcd@1234',
   })
   password: string;
 }
